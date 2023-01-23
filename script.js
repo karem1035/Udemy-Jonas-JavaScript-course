4c7dc81327d256995ae72b84f11ba1b051c5ce47
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-01-23 07:16:28 +0200

    LECTURE: Values and Variables

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// assignments-js-fundamentals
+// JavaScript Fundamentals – Part 1
+
+// LECTURE: Values and Variables
+
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+const country = "Egypt";
+const continent = "Africa";
+const population = 100;
+
+console.log(country, continent, population);
+
+//////////////////////////////////////////////////////////////////
