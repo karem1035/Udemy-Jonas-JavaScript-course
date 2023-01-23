482185bd959c619b21b535a201fb977f7a75b3c5
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-01-23 07:21:12 +0200

    Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // assignments-js-fundamentals
 // JavaScript Fundamentals – Part 1
 
@@ -9,6 +9,7 @@
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
+*/
 const country = "Egypt";
 const continent = "Africa";
 const population = 100;
@@ -16,3 +17,16 @@ const population = 100;
 console.log(country, continent, population);
 
 //////////////////////////////////////////////////////////////////
+
+// LECTURE: Data Types
+
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+const isIsland = false;
+const language;
+
+console.log(isIsland,country, continent, population, language);
